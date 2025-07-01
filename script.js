65a8bbd454779301f928941c35a8c1c5a175e9ff
GreenQwest24 <61900622+GreenQwest24@users.noreply.github.com>
2025-07-01 10:36:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// toggle the menu button
+function toggleMenu(){
+    const menu = document.querySelector('menu')
+    const nav = document.querySelector('.nav')
+    menu.classList.toggle('active');
+    nav.class
+
+}
